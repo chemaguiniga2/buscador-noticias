cadb2da41eb09e0b1f542b7e6ba247b8960561e2
chemaguiniga2 <A01376669@itesm.mx>
2021-09-30 17:31:58 -0500

    end

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/header';
+import ListadoNoticias from './components/ListadoNoticias';
+
+
+
+
+
+function App() {
+
+  //definir categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=a5b04cb21cd4445f80202bde269d11d6`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+
+    }
+    consultarAPI();
+  }, [categoria]);
+  
+  return (
+    <Fragment>
+      <Header
+        titulo="Buscador de Noticias"
+      />
+      <div className="container white">
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
+        <ListadoNoticias
+          noticias={noticias}
+        />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
